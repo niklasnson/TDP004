b5bef95a18803074eb34a660095790b64fbeec5b
Niklas Nson <niklasnson@me.com>
2015-10-29 14:24:54 +0100

    testing variables

---
+++ from_the_book/ass_variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std; 
+
+int main() 
+{
+  int a= 3;
+  a= 4; 
+  int b= a;
+  b= a+5; 
+  a= a+7;
+
+  cout << a;
+
+  int y= 8;
+  int x= 9; 
+
+  string t= "howdy";
+  string s= "G'day";
+
+  cout << t;
+  cout << s;
+}
